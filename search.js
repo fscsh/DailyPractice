4a109a72519cb1c0b602ddfd23e0547fb0e25115
Shuhao Chen <shuhao.chen@sjsu.edu>
2017-11-19 16:19:51 -0800

    add duplicate

--- search.js
+++ search.js
@@ -36,6 +36,33 @@ function helper(nums, target, res, left, right) {
     return res[0];
 }
 
-let nums = [1];
-let target = 1;
-console.log(search(nums, target));
+//duplicate case
+function search(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        console.log('left = ', left, 'right = ', right, 'mid =', mid);
+        if (nums[mid] === target) {
+            return true;
+        }
+        if (nums[mid] > nums[left]) {
+            console.log('mid>left');
+            if (target >= nums[left] && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else if(nums[mid]< nums[left]){
+            console.log('mid<right');
+            if (target > nums[mid] && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }else {
+            left++;
+        }
+    }
+    return false;
+}
